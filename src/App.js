5dccf195bc88ee6450241367590c7bf8a5118670
Jake Hathaway <77807678+th3oth3rjak3@users.noreply.github.com>
2022-07-29 07:31:25 -0700

    Moved navbar and footer up to app.js for lazyload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
-import AppBody from "./components/app-body/app-body";
+import React from "react";
+import NavBar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import { ClockLoader } from "react-spinners";
+const AppBody = React.lazy(() => import("./components/app-body/app-body"));
 
 function App() {
-	return <AppBody className="App"></AppBody>;
+	return (
+		<>
+			<NavBar />
+			<React.Suspense
+				fallback={
+					<ClockLoader
+						color={"#36D7B7"}
+						cssOverride={{
+							display: "block",
+							margin: " 100px auto",
+						}}
+					/>
+				}
+			>
+				<AppBody className="App" />
+			</React.Suspense>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/components/app-body/app-body.js
+++ src/components/app-body/app-body.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./app-body.css";
-import NavBar from "../../components/navbar/navbar";
-import Footer from "../../components/footer/footer";
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
-import { ClockLoader } from "react-spinners";
 const AddRecipe = React.lazy(() => import("../../pages/AddRecipe/AddRecipe"));
 const MyRecipes = React.lazy(() => import("../../pages/MyRecipes/MyRecipes"));
 const Home = React.lazy(() => import("../../pages/Home/Home"));
@@ -18,36 +15,20 @@ const EditRecipe = React.lazy(() =>
 
 function AppBody() {
 	return (
-		<>
-			<NavBar></NavBar>
-			<Container className="body">
-				<React.Suspense
-					fallback={
-						<ClockLoader
-							color={"#36D7B7"}
-							cssOverride={{
-								display: "block",
-								margin: "0 auto",
-							}}
-						/>
-					}
-				>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/MyRecipes" element={<MyRecipes />} />
-						<Route path="/MyRecipes/:search" element={<MyRecipes />} />
-						<Route
-							path="/MyRecipes/RecipeDetails/:id"
-							element={<RecipeDetails />}
-						/>
-						<Route path="/MyRecipes/EditRecipe/:id" element={<EditRecipe />} />
-						<Route path="/AddRecipe" element={<AddRecipe />} />
-						<Route path="/Help" element={<Help />} />
-					</Routes>
-				</React.Suspense>
-			</Container>
-			<Footer></Footer>
-		</>
+		<Container className="body">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/MyRecipes" element={<MyRecipes />} />
+				<Route path="/MyRecipes/:search" element={<MyRecipes />} />
+				<Route
+					path="/MyRecipes/RecipeDetails/:id"
+					element={<RecipeDetails />}
+				/>
+				<Route path="/MyRecipes/EditRecipe/:id" element={<EditRecipe />} />
+				<Route path="/AddRecipe" element={<AddRecipe />} />
+				<Route path="/Help" element={<Help />} />
+			</Routes>
+		</Container>
 	);
 }
 
